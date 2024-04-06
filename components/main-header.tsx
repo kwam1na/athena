ac91767999f9083151aac9f8eb081d1480f5447d
kwamina <kwamina.0x00@gmail.com>
2024-04-06 19:01:28 -0400

    remove blur

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -7,7 +7,7 @@ export const MainHeader = async () => {
    const stores = await fetchStores(parseInt('1'));
 
    return (
-      <div className="w-full h-16 flex items-center justify-between border-b px-4 backdrop-blur-md bg-opacity-30 justify-between fixed top-0 z-40">
+      <div className="w-full h-16 flex items-center justify-between border-b px-4 bg-background justify-between fixed top-0 z-40">
          <div className="flex items-center gap-4">
             <Link href={'/'} className="flex items-center">
                <p className="text-sm font-medium">athena</p>
--- components/ui/inner-header.tsx
+++ components/ui/inner-header.tsx
@@ -1,6 +1,6 @@
 export const InnerHeader = ({ children }: { children: React.ReactNode }) => {
    return (
-      <div className="ml-4 w-full h-12 flex items-center py-8 pr-24 backdrop-blur-md bg-opacity-30 fixed top-16 z-30">
+      <div className="ml-4 w-full h-12 flex items-center py-8 pr-24 bg-background fixed top-16 z-30">
          {children}
       </div>
    );
