97438018de1234f0692dc0ea2e807ad41dac237c
Kwamina <47408981+kwam1na@users.noreply.github.com>
2026-04-16 15:36:03 -0400

    [V26-261] Pin TanStack Start server resolution (#155)

--- packages/storefront-webapp/vite.config.ts
+++ packages/storefront-webapp/vite.config.ts
@@ -42,10 +42,17 @@ export default defineConfig({
     alias: {
       "~": __dirname,
       "@": path.resolve(__dirname, "./src"),
-      "@tanstack/start": path.resolve(__dirname, "../../node_modules/@tanstack/start"),
+      "@tanstack/start": path.resolve(
+        __dirname,
+        "../../node_modules/@tanstack/start/dist/esm/client.js",
+      ),
+      "@tanstack/start/server": path.resolve(
+        __dirname,
+        "../../node_modules/@tanstack/start/dist/esm/server.js",
+      ),
       "@tanstack/react-router": path.resolve(
         __dirname,
-        "../../node_modules/@tanstack/react-router",
+        "../../node_modules/@tanstack/react-router/dist/esm/index.js",
       ),
     },
   },
